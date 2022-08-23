24a4323679c1fd4426d251fd00081fec0fc7f4fb
somemacsongs <pedromacenac@gmail.com>
2022-08-23 02:24:32 -0300

    index first draft

---
+++ src/questions.js
@@ -0,0 +1,192 @@
+const questions = [
+    {
+        question: "What's Bitta's last name?",
+        op1: "Edison",
+        op2: "Bennet",
+        op3: "Barry",
+        op4: "Barnes",
+        correct: "Perry",
+        stage: 1
+    },
+    {
+        question: "How many episodes does season 1 have?",
+        op1: "21",
+        op2: "18",
+        op3: "22",
+        op4: "24",
+        correct: "25",
+        stage: 1
+    },
+    {
+        question: "What's Abed's racial background?",
+        op1: "Indian and Polish",
+        op2: "Arab and German",
+        op3: "Indian and Slovenian",
+        op4: "Arab and Indian",
+        correct: "Arab and Polish",
+        stage: 1
+    },
+    {
+        question: "What does Pierce's dad's company sells?",
+        op1: "Napkins",
+        op2: "Toilette Paper",
+        op3: "Dish Towels",
+        op4: "Sanitizing Wipes",
+        correct: "Moist Towelettes",
+        stage: 1
+    },
+    {
+        question: "What is the name of the app students of Greendale used to rate each other?",
+        op1: "WoofWoofBeenz",
+        op2: "GreedaleStars",
+        op3: "FiveStars",
+        op4: "CheeChoowCheeChoow",
+        correct: "MeowMeowBeenz",
+        stage: 1
+    },
+    {
+        question: "What is the Dean's weird fetish?",
+        op1: "Brazilian Birds",
+        op2: "German Shepards",
+        op3: "Jeff",
+        op4: "Labradors",
+        correct: "Dalmatians",
+        stage: 1
+    },
+    {
+        question: "How many children does Shirley have?",
+        op1: "1",
+        op2: "2",
+        op3: "4",
+        op4: "None",
+        correct: "3",
+        stage: 1
+    },
+    {
+        question: "What is Abed's girlfirend's name?",
+        op1: "Raquel",
+        op2: "Rosie",
+        op3: "Ruth",
+        op4: "Rita",
+        correct: "Rachel",
+        stage: 1
+    },
+    {
+        question: "What is Troy's monkey's name?",
+        op1: "King Kong",
+        op2: "Britta's Boobs",
+        op3: "Fivel",
+        op4: "Bruno",
+        correct: "Annie's Boobs",
+        stage: 1
+    },
+    {
+        question: "What is the name of the interactive VCR game Abed and Annie play?",
+        op1: "Dungeons and Dragons",
+        op2: "Pile of Guns",
+        op3: "Whose Ears are These?",
+        op4: "Bang!",
+        correct: "Pile of Bullets",
+        stage: 1
+    },
+    {
+        question: "What's Abed's favorite food?",
+        op1: "Salmon",
+        op2: "Splingles",
+        op3: "Kettle Corn",
+        op4: "Pizza",
+        correct: "Buttered Noodles",
+        stage: 1
+    },
+    {
+        question: "What's the name of Greendale's most dangerous criminal?",
+        op1: "Butt Crack Psycho",
+        op2: "Coin Bandit",
+        op3: "Ass Cheek Robber",
+        op4: "Benjamin Chang",
+        correct: "Ass Crack Bandit",
+        stage: 1
+    },
+    {
+        question: "Who are the winners of the first and sacond Paintball game, respectively?",
+        op1: "Jeff and Jeff",
+        op2: "Britta and Pierce",
+        op3: "Jeff and Shirley",
+        op4: "Chang and Shirley",
+        correct: "Jeff and Pierce",
+        stage: 1
+    },
+    {
+        question: "What's the name of Greendale's drama teacher?",
+        op1: "Professor Professorson",
+        op2: "Professor Peter Sheffield",
+        op3: "Professor Ian Duncan",
+        op4: "Professor Noel Cornwallis",
+        correct: "Professor Sean Garrity",
+        stage: 1
+    },
+    {
+        question: "Which member of the Study Group is, according to Pierce, his favorite?",
+        op1: "Jeff",
+        op2: "Shirley",
+        op3: "Troy",
+        op4: "Britta",
+        correct: "Annie",
+        stage: 1
+    },
+    {
+        question: "Where is Rich, from pottery class, from?",
+        op1: "Denver",
+        op2: "New Jersey",
+        op3: "Albuquerque",
+        op4: "San Francisco",
+        correct: "Santa Fe",
+        stage: 1
+    },
+    {
+        question: "How many times has Pierce been married?",
+        op1: "Three",
+        op2: "One",
+        op3: "Five",
+        op4: "Eight",
+        correct: "Seven",
+        stage: 1
+    },
+    {
+        question: "Who is Troy's favorite celebrity?",
+        op1: "Jim Belushi",
+        op2: "Chevy Chase",
+        op3: "Cristian Bale",
+        op4: "Sophie B Hawkins",
+        correct: "LeVar Burton",
+        stage: 1
+    },
+    {
+        question: "Which fast food chain (almost) buys Greendale?",
+        op1: "McDonald's",
+        op2: "KFC",
+        op3: "In-N-Out Burger",
+        op4: "Taco Bell",
+        correct: "Subway",
+        stage: 1
+    },
+    {
+        question: "What's Abed's favortite TV show?",
+        op1: "Cougar Town",
+        op2: "Cougarton Abbey",
+        op3: "The Cape",
+        op4: "Doctor Who",
+        correct: "Inspector Spacetime",
+        stage: 1
+    },
+    {
+        question: "",
+        op1: "",
+        op2: "",
+        op3: "",
+        op4: "",
+        correct: "",
+        stage: 2
+    }
+
+]
