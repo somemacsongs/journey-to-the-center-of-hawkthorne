128198e403fa343a8d27d6c77ff4a0b5ef37bfac
somemacsongs <pedromacenac@gmail.com>
2022-08-25 00:27:00 -0300

    game.js versao final

--- src/game.js
+++ src/game.js
@@ -11,10 +11,12 @@ let acceptingAnswers = true;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
+let stageOne = [];
+let stageTwo = [];
 
 const questions = [
     {
-        question: "What's Bitta's last name?",
+        question: "What's Britta's last name?",
         op1: "Perry",
         op2: "Bennet",
         op3: "Barry",
@@ -134,7 +136,7 @@ const questions = [
         stage: 1
     },
     {
-        question: "Who are the winners of the first and sacond Paintball game, respectively?",
+        question: "Who are the winners of the first and second Paintball game, respectively?",
         op1: "Jeff and Jeff",
         op2: "Britta and Pierce",
         op3: "Jeff and Shirley",
@@ -212,16 +214,79 @@ const questions = [
         op5: "Doctor Who",
         correct: 2,
         stage: 1
+    },
+    {
+        question: "How did Chang become famous?",
+        op1: "He ate a person",
+        op2: "He made a comercial where he says 'Ham, Girl!'",
+        op3: "He was the first asian keytarist to win a Grammy ",
+        op4: "He starred in the new Play-Doh movie as the color Blue",
+        op5: "He was the first person to have Changnesia",
+        correct: 2,
+        stage: 2
+    },
+    {
+        question: "What event helps Briita decide she wants to major in Psychology?",
+        op1: "Britta helps Jeff reconnect with his dad",
+        op2: "Briita helps Pierce with his pill addiction",
+        op3: "Britta helps Abed go to a happy place in his mind?",
+        op4: "Britta helps Abed find a new favorite TV show",
+        op5: "Britta helps Annie say the word 'Penis'",
+        correct: 4,
+        stage: 2
+    },
+    {
+        question: "Which class was canceled by Frankie when she arrived at Greendale?",
+        op1: "Can I Fry That?",
+        op2: "History of Ice Cream",
+        op3: "Conspiracy Theories",
+        op4: "Nicolas Cage: Good or Bad?",
+        op5: "Ladders",
+        correct: 5,
+        stage: 2
+    },
+    {
+        question: "How does the Save Greendale Committee save Greendale from being bought?",
+        op1: "They find a secret treasure",
+        op2: "Pierce lends them money",
+        op3: "They find the deeds of the property",
+        op4: "They plan an elaborate heist",
+        op5: "They replace the Dean for a 'Doppledeaner'",
+        correct: 3,
+        stage: 2
+    },
+    {
+        question: "When do Jeff and Annie have their first kiss?",
+        op1: "Troy and Abed's new appartment party",
+        op2: "Last night of school of the first year",
+        op3: "The debate 'Man is good vs. Man is evil'",
+        op4: "Last episode",
+        op5: "Battle of model UN's",
+        correct: 3,
+        stage: 2
+    },
+    {
+        question: "What is Troy/Abed/Annie's apartment number?",
+        op1: "203",
+        op2: "303",
+        op3: "102",
+        op4: "101",
+        op5: "202",
+        correct: 2,
+        stage: 1
     }
 ]
 
 const scorePoints = [1,2,3];
-const maxQuestions = 20;    
+const maxQuestions = 30;    
 
 startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
+    stageOne = availableQuestions.filter((current) => current.stage === 1);
+    stageTwo = availableQuestions.filter((current) => current.stage === 2);
+    stageThree = availableQuestions.filter((current) => current.stage === 3);
     getNewQuestion ();
 }
 
@@ -231,21 +296,49 @@ getNewQuestion = () => {
         return window.location.assign("./end.html");
     }
     questionCounter++;
+    console.log(questionCounter);
     progressText.innerText = `${questionCounter} of ${maxQuestions}`;
     progressBarFull.style.width = `${(questionCounter/maxQuestions) * 100}%`;
 
-    const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionsIndex];
-    question.innerText = currentQuestion.question;
+    if (questionCounter<=10){
+        const questionsIndex = Math.floor(Math.random() * stageOne.length);
+        currentQuestion = stageOne[questionsIndex];
+        question.innerText = currentQuestion.question;
 
-    choices.forEach((choice) => {
-        const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['op'+number];
-    });
+        choices.forEach((choice) => {
+            const number = choice.dataset['number'];
+            choice.innerText = currentQuestion['op'+number];
+        });
+    
+        stageOne.splice(questionsIndex, 1);
+        acceptingAnswers = true;
+    }
+    if (questionCounter<=20 && questionCounter>10){
+        const questionsIndex = Math.floor(Math.random() * stageTwo.length);
+        currentQuestion = stageTwo[questionsIndex];
+        question.innerText = currentQuestion.question;
 
-    availableQuestions.splice(questionsIndex, 1);
+        choices.forEach((choice) => {
+            const number = choice.dataset['number'];
+            choice.innerText = currentQuestion['op'+number];
+        });
+    
+        stageTwo.splice(questionsIndex, 1);
+        acceptingAnswers = true;
+    }
+    if (questionCounter<=30 && questionCounter>20){
+        const questionsIndex = Math.floor(Math.random() * stageThree.length);
+        currentQuestion = stageThree[questionsIndex];
+        question.innerText = currentQuestion.question;
 
-    acceptingAnswers = true;
+        choices.forEach((choice) => {
+            const number = choice.dataset['number'];
+            choice.innerText = currentQuestion['op'+number];
+        });
+    
+        stageThree.splice(questionsIndex, 1);
+        acceptingAnswers = true;
+    }
 }
 
 choices.forEach((choice) => {
